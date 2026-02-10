304bd4c17abe47ef4af116e266cf344e821285e5
Pablo Zamora <PFZAMORA@Correo.url.edu.gt>
2026-02-09 18:35:25 -0600

    agrega pruebas automatizadas y CI

--- o	api/saludo.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const nombre = req.query.nombre || "anónimo";
-
-  res.status(200).json({
-    mensaje: `Hola ${nombre}`,
-    curso: "Ingeniería de Software II",
-    timestamp: new Date().toISOString()
-  });
-}
