df650dc7248bb2ceb57edb4091b1f8a25ca63ee8
Pablo Zamora <PFZAMORA@Correo.url.edu.gt>
2026-02-09 18:59:19 -0600

    Agregar longitud

--- api/procesar.js
+++ api/procesar.js
@@ -2,6 +2,8 @@ export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+    longitud: nombre.length
   });
 }
+
